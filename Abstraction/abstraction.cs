445f8fc0efaf510e627a271e34f5e02727014fc0
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-10-22 16:49:13 +0500

    Abstraction in c#

---
+++ Abstraction/abstraction.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace abstraction
+{
+    abstract class Animal
+    {
+        public abstract void AnimalSound();
+        public void Sleep()
+        {
+            Console.WriteLine("This is animal is sleeping!");
+        }
+    }
+    class Lion : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The lion is roaring!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion myLion = new Lion();
+            myLion.AnimalSound();
+            myLion.Sleep();
+
+        }
+    }
+}
