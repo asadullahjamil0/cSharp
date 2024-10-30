33d68da140fa576aa4a12c3f1b99e7f6d727423b
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-10-30 20:45:28 +0500

    Access Modifier

--- Function overloading and function overriding/program.cs
+++ Function overloading and function overriding/program.cs
@@ -1,59 +1,59 @@
-using System;
+// using System;
 
-namespace overloading
-{
+// namespace overloading
+// {
 
-    public class Program
-    {
-        static int Sum(int x, int y)
-        {
-            return x + y;
-        }
-        static float Sum(int x, float y)
-        {
-            return x + y;
-        }
-        static double Sum(double x, double y)
-        {
-            return x + y;
-        }
+//     public class Program
+//     {
+//         static int Sum(int x, int y)
+//         {
+//             return x + y;
+//         }
+//         static float Sum(int x, float y)
+//         {
+//             return x + y;
+//         }
+//         static double Sum(double x, double y)
+//         {
+//             return x + y;
+//         }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine($"We'll learn about function overloading");
-            Console.WriteLine("First int method is: " + Sum(23, 45));
-            Console.WriteLine("Second Float method is: " + Sum(53, 82.34f));
-            Console.WriteLine("Third double method is: " + Sum(70.423, 91.532));
-            Console.WriteLine($"We'll learn about function Overriding");
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine($"We'll learn about function overloading");
+//             Console.WriteLine("First int method is: " + Sum(23, 45));
+//             Console.WriteLine("Second Float method is: " + Sum(53, 82.34f));
+//             Console.WriteLine("Third double method is: " + Sum(70.423, 91.532));
+//             Console.WriteLine($"We'll learn about function Overriding");
 
-            Dog dog = new Dog();
-            dog.speak();
+//             Dog dog = new Dog();
+//             dog.speak();
 
-            Cat cat = new Cat();
-            cat.speak();
+//             Cat cat = new Cat();
+//             cat.speak();
 
-        }
-        class Animal
-        {
-            public virtual void speak()
-            {
-                Console.WriteLine("Animal goes to *brrr*");
-            }
+//         }
+//         class Animal
+//         {
+//             public virtual void speak()
+//             {
+//                 Console.WriteLine("Animal goes to *brrr*");
+//             }
 
-        }
-        class Dog : Animal
-        {
-            public override void speak()
-            {
-                Console.WriteLine($"The dog goes to *Woof*");
-            }
-        }
-        class Cat : Animal
-        {
-            public override void speak()
-            {
-                Console.WriteLine($"The cat goes to *meow*");
-            }
-        }
-    }
-}
+//         }
+//         class Dog : Animal
+//         {
+//             public override void speak()
+//             {
+//                 Console.WriteLine($"The dog goes to *Woof*");
+//             }
+//         }
+//         class Cat : Animal
+//         {
+//             public override void speak()
+//             {
+//                 Console.WriteLine($"The cat goes to *meow*");
+//             }
+//         }
+//     }
+// }
--- obj/Debug/net8.0/cSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b4fdec50069563fa421d68d1d69e327c517b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a2e55c09d3b8cc27170122da6e9382bbad42cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
